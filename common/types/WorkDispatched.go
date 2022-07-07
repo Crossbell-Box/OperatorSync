a306060afd10ba640edac62082e396d14edb4e70
Nya Candy <dev@candinya.com>
2022-07-07 12:54:40 +0800

    fix: field name error

--- common/types/WorkDispatched.go
+++ common/types/WorkDispatched.go
@@ -8,5 +8,5 @@ type WorkDispatched struct {
 	Platform    string    `json:"platform"`
 	CollectLink string    `json:"collect_link"` // HTTP(S) link to the RSS endpoint
 	DropBefore  time.Time `json:"drop_before"`  // Ignore feeds before last updated time
-	DropAfter   time.Time `yaml:"drop_after"`   // Ignore feeds after next updated time
+	DropAfter   time.Time `json:"drop_after"`   // Ignore feeds after next updated time
 }
