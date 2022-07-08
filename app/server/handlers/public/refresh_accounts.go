5cebe5911d725c7ff497e8d331da129981eeadf0
Nya Candy <dev@candinya.com>
2022-07-08 10:40:39 +0800

    feat(wip): get accounts from crossbell

--- app/server/handlers/public/refresh_accounts.go
+++ app/server/handlers/public/refresh_accounts.go
@@ -7,6 +7,7 @@ import (
 	"github.com/Crossbell-Box/OperatorSync/app/server/consts"
 	"github.com/Crossbell-Box/OperatorSync/app/server/global"
 	"github.com/Crossbell-Box/OperatorSync/app/server/models"
+	"github.com/Crossbell-Box/OperatorSync/app/server/utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
@@ -40,11 +41,62 @@ func RefreshAccounts(ctx *gin.Context) {
 	}
 
 	// Start flush account work
+	var dbAccounts []models.Account
+
+	// Get accounts from database
+	global.DB.
+		Model(&models.Account{}).
+		Order("created_at").
+		Find(&dbAccounts)
+
+	// Get accounts from CrossBell
+	csbAccounts, err := utils.GetAccountFromCrossbell(reqCharacter)
+	if err != nil {
+		// Internal server error
+		global.Logger.Error("Failed to get accounts from CSB for (", reqCharacter, ") : ", err.Error())
+		ctx.JSON(http.StatusOK, gin.H{
+			"ok":      false,
+			"message": "Internal server error: " + err.Error(),
+		})
+		return
+	}
+
+	// Prepare accounts
 	var accounts []models.Account
 
-	// TODO: Finish this
+	dbAccountsShouldKeep := make([]bool, len(dbAccounts))
+	csbAccountsAlreadyExist := make([]bool, len(csbAccounts))
 
-	// Reset cache key
+	/// Hint: Golang would initialize memory automatically, so there's no need to manually set all member to `false`
+
+	// Check accounts
+	for csbI, accountFromCSB := range csbAccounts {
+		accounts = append(accounts, models.Account{
+			Account: accountFromCSB,
+		})
+		for dbI, accountFromDB := range dbAccounts {
+			if accountFromDB.Platform == accountFromCSB.Platform && accountFromDB.Username == accountFromCSB.Username {
+				csbAccountsAlreadyExist[csbI] = true
+				dbAccountsShouldKeep[dbI] = true
+			}
+		}
+	}
+
+	// Delete non-exist accounts
+	for dbI, shouldKeep := range dbAccountsShouldKeep {
+		if !shouldKeep {
+			global.DB.Delete(&dbAccounts[dbI])
+		}
+	}
+
+	// Insert new accounts
+	for csbI, alreadyExist := range csbAccountsAlreadyExist {
+		if !alreadyExist {
+			global.DB.Create(&csbAccounts[csbI])
+		}
+	}
+
+	// Cache key
 	listAccountsCacheKey := fmt.Sprintf("%s:%s:%s", consts.CACHE_PREFIX, "accounts:list", reqCharacter)
 
 	// Set cache
---
+++ app/server/utils/get_accounts_from_crossbell.go
@@ -0,0 +1,9 @@
+package utils
+
+import "github.com/Crossbell-Box/OperatorSync/app/server/types"
+
+func GetAccountFromCrossbell(characterHandle string) ([]types.Account, error) {
+	// TODO: Get accounts
+
+	return nil, nil
+}
