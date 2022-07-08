13e64e6b550c955ed22ddb4d31ae03d2f7a3adc8
Nya Candy <dev@candinya.com>
2022-07-08 11:18:07 +0800

    feat: add MQ note

--- app/server/inits/mq.go
+++ app/server/inits/mq.go
@@ -8,10 +8,13 @@ import (
 )
 
 func MQ() error {
+	// Note: NATS would drop works if there's no consumer to accept, but it's OK for now.
+	// If there's further need for those, using JetStream mode might be better.
+
 	var err error
 
 	global.MQ, err = nats.Connect(config.Config.MQConnString)
-	
+
 	if err != nil {
 		return fmt.Errorf("unable to connect to mq: %w", err)
 	}
