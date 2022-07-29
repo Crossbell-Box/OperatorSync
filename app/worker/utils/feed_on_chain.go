226b4c0941a45af238e2a6d0de25539cd50a1437
Nya Candy <dev@candinya.com>
2022-07-29 23:53:43 +0800

    chore: fix on-chain steps

--- app/worker/utils/feed_on_chain.go
+++ app/worker/utils/feed_on_chain.go
@@ -5,10 +5,12 @@ import commonTypes "github.com/Crossbell-Box/OperatorSync/common/types"
 func FeedOnChain(work *commonTypes.OnChainDispatched) (string, error) {
 	// TODO : Finish this
 
-	// Step 1: Parse feeds to note metadata and upload to IPFS
+	// Step 1: Parse feeds to note metadata
 
-	// Step 2: Upload feeds to Crossbell Chain
+	// Step 2: Upload note metadata to IPFS, get IPFS Uri as note ContentUri
 
-	// Step 3: Return Transaction hash and done!
+	// Step 3: Upload note to Crossbell Chain with ContentUri
+
+	// Step 4: Return Transaction hash and done!
 	return "tx", nil
 }
