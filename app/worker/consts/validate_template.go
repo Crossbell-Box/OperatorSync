1fdf82b76bfcdde3ef1b75a1fa3a4aa0bad7745f
Nya Candy <dev@candinya.com>
2022-07-29 21:38:53 +0800

    feat: remove unused validate template string

--- app/worker/consts/validate_template.go
+++
@@ -1,9 +0,0 @@
-package consts
-
-// Validate Template
-//// {{crossbell_character_handle}}: Crossbell Character Handle
-//// {{crossbell_character_id}}: Crossbell Character ID
-
-const (
-	ValidateTemplate = "Crossbell@{{crossbell_character_handle}}#{{crossbell_character_id}}"
-)
