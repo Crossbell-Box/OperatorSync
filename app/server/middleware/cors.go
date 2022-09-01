f1cf9cf1003bac4bf2ca3ef10c22feae16ad773b
Nya Candy <dev@candinya.com>
2022-09-01 16:47:17 +0800

    fix: CORS missing methods

--- app/server/middleware/cors.go
+++ app/server/middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func PublicCORS() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		ctx.Header("Access-Control-Allow-Origin", "*")
-		ctx.Header("Access-Control-Allow-Methods", "GET, OPTIONS")
+		ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		ctx.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 		ctx.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
 		ctx.Header("Access-Control-Allow-Credentials", "true")
