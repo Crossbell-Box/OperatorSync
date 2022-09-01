5ee0c8c67c818c8e79d90c03b3bf2289ef54eb13
Nya Candy <dev@candinya.com>
2022-09-01 16:29:11 +0800

    fix: unpatched template string

--- app/worker/platforms/tiktok/account.go
+++ app/worker/platforms/tiktok/account.go
@@ -1,6 +1,7 @@
 package tiktok
 
 import (
+	"github.com/Crossbell-Box/OperatorSync/app/worker/config"
 	"github.com/Crossbell-Box/OperatorSync/app/worker/global"
 	"github.com/Crossbell-Box/OperatorSync/app/worker/jobs/callback"
 	"github.com/Crossbell-Box/OperatorSync/app/worker/utils"
@@ -15,8 +16,21 @@ func Account(mqReply string, username string, validateString string) {
 	// RSSHub will index userinfo, so we need to parse their feeds
 	// Sorry TikTok server :pray:
 
+	collectLink := commonConsts.SUPPORTED_PLATFORM["tiktok"].FeedLink
+
+	collectLink =
+		strings.ReplaceAll(
+			strings.ReplaceAll(
+				collectLink,
+				"{{rsshub_stateful}}",
+				config.Config.RSSHubEndpointStateful,
+			),
+			"{{rsshub_stateless}}",
+			config.Config.RSSHubEndpointStateless,
+		)
+
 	if rawFeed, errCode, err := utils.RSSFeedRequest(
-		strings.ReplaceAll(commonConsts.SUPPORTED_PLATFORM["tiktok"].FeedLink, "{{username}}", username),
+		strings.ReplaceAll(collectLink, "{{username}}", username),
 		true,
 	); err != nil {
 		global.Logger.Error("Failed to collect tiktok feeds of user ", username, " for account validate with error: ", err.Error())
