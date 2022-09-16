4eefebf255ad5bec97f490965a6477d3c15bf88b
Nya Candy <dev@candinya.com>
2022-09-16 23:42:38 +0800

    feat: extraordinarily shorten refresh gap

--- common/consts/platform.go
+++ common/consts/platform.go
@@ -19,8 +19,8 @@ var SUPPORTED_PLATFORM = map[string]platform{
 	"medium": {
 		Name:               "Medium",
 		FeedLink:           "https://medium.com/feed/@{{username}}",
-		MinRefreshGap:      3 * time.Hour,
-		MaxRefreshGap:      7 * 24 * time.Hour,
+		MinRefreshGap:      5 * time.Minute,
+		MaxRefreshGap:      10 * time.Minute,
 		IsMediaAttachments: false,
 	},
 	"tiktok": {
