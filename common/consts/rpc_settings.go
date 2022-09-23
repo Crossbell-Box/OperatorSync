63d89eb43869167d8a6abdb99b8bb4bc733e0c18
Nya Candy <dev@candinya.com>
2022-09-23 11:11:33 +0800

    chore: enlarge rpc request timeout for tiktok

--- common/consts/rpc_settings.go
+++ common/consts/rpc_settings.go
@@ -7,7 +7,7 @@ const (
 	RPCSETTINGS_BaseServiceName = "COSWorkerV2"
 
 	RPCSETTINGS_ValidateServiceName    = "Validate" // Should be same as function name
-	RPCSETTINGS_ValidateRequestTimeOut = 10 * time.Second
+	RPCSETTINGS_ValidateRequestTimeOut = 20 * time.Second
 
 	RPCSETTINGS_OnChainServiceName    = "OnChain" // Should be same as function name
 	RPCSETTINGS_OnChainRequestTimeOut = 3 * time.Minute
