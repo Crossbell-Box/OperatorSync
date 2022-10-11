1219fe7e6b18fcceee1428a53d558d9c8c2b9b1a
Nya Candy <dev@candinya.com>
2022-10-11 16:17:15 +0800

    fix: recover primary key

--- app/server/types/Character.go
+++ app/server/types/Character.go
@@ -2,7 +2,7 @@ package types
 
 type Character struct {
 	// Structure related
-	CrossbellCharacterID string `json:"crossbell_character_id" gorm:"column:crossbell_character_id"`
+	CrossbellCharacterID string `json:"crossbell_character_id" gorm:"primarykey;column:crossbell_character_id"`
 
 	//// Accounts related
 	//MediaUsage MediaTypeRecordArray `gorm:"type:text" json:"media_usage"`
